fafa412d4dd7cae6f86dcc903946abc1ce51ac03
C-Lo44 <154693249+C-Lo44@users.noreply.github.com>
2025-12-09 19:14:07 -0500

    Delete LightingPortolfio directory

--- LightingPortolfio/Sections/About.js
+++
@@ -1,4 +0,0 @@
-export function init(root) {
-  // Optional: add About-specific behavior here
-  console.log("About tab loaded");
-}
--- LightingPortolfio/Sections/Contact.js
+++
@@ -1,4 +0,0 @@
-export function init(root) {
-  console.log("Contact tab loaded");
-  // You could add a form handler or animations here if you want
-}
