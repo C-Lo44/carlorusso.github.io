c79abd42c8af4d2094e23ecc277fa7eb4a3615a0
CarloR44 <148703568+CarloR44@users.noreply.github.com>
2026-01-27 14:01:46 -0500

    Sending All Updating Pages

--- Sections/About.js
+++ Sections/About.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 export function init(root) {
   // Optional: add About-specific behavior here
   console.log("About tab loaded");
 }
-=======
-export function init(root) {
-  // Optional: add About-specific behavior here
-  console.log("About tab loaded");
-}
->>>>>>> Testing
--- Sections/Contact.js
+++ Sections/Contact.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 export function init(root) {
   console.log("Contact tab loaded");
   // You could add a form handler or animations here if you want
 }
-=======
-export function init(root) {
-  console.log("Contact tab loaded");
-  // You could add a form handler or animations here if you want
-}
->>>>>>> Testing
